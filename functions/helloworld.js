956f4c7b82b533881e0edd0c781d4f358f147249
Blugamma <blugammadesign@gmail.com>
2023-09-24 15:42:46 +0100

    added d1

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,6 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export async function onRequest(context) {
+    const ps = context.env.DB.prepare('SELECT * from players');
+    const data = await ps.first();
+
+    return Response.json(data);
   }
